a481276a33bbd03010903339458843952f49ea32
omar suliman abu sabha <ssomar2028@gmail.com>
2020-04-10 20:27:12 +0300

    9th day : design a stack which supports puch ,pop, getMin => O(1), and top problem

--- src/com/company/Contests/Week_2_leetCodeContest_30Days.java
+++ src/com/company/Contests/Week_2_leetCodeContest_30Days.java
@@ -1,12 +1,32 @@
 package com.company.Contests;
 
+import java.util.Stack;
+
 public class Week_2_leetCodeContest_30Days {
 
 
     public static void main(String[] args) {
 
         // 8th day
-        System.out.println(backspaceCompare("ab#c", "ad#c"));
+        // System.out.println(backspaceCompare("ab#c", "ad#c"));
+
+
+        // 9th day :
+        // Your MinStack object will be instantiated and called as such:
+        /*
+               MinStack obj = new MinStack();
+               int[] arr = {1,2,3};
+               for (int i = 0; i < arr.length; i++) {
+                    obj.push(arr[i]);
+                }
+               obj.pop();
+               int param_3 = obj.top();
+               int param_4 = obj.getMin();
+                System.out.println("param_3 = " + param_3);
+                System.out.println("param_4 = " + param_4);
+        */
+
+
     }
 
 
@@ -57,7 +77,6 @@ public class Week_2_leetCodeContest_30Days {
      * # means a backspace character.
      * */
 
-
     public static boolean backspaceCompare(String S, String T) {
         return strWithOutHash(S).equals(strWithOutHash(T));
     }
@@ -74,6 +93,48 @@ public class Week_2_leetCodeContest_30Days {
         return buf.toString();
     }
 
+    /** 9th day
+     *Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+     * push(x) -- Push element x onto stack.
+     * pop() -- Removes the element on top of the stack.
+     * top() -- Get the top element.
+     * getMin() -- Retrieve the minimum element in the stack.
+     * */
+    static class MinStack {
+        Stack<Integer> stack1;
+        Stack<Integer> stack2;
+
+        /** initialize your data structure here. */
+        public MinStack() {
+            stack1 =  new Stack<>();
+            stack2 =  new Stack<>();
+        }
+
+        public void push(int x) {
+            stack1.push(x);
+            if(stack2.isEmpty()){
+                stack2.push(x);
+            }else{
+                stack2.push(Math.min(x,stack2.peek()));
+            }
+
+        }
+
+        public void pop() {
+            stack1.pop();
+            stack2.pop();
+
+        }
+
+        public int top() {
+            return stack1.peek();
+
+        }
+
+        public int getMin() {
+            return stack2.peek();
+        }
+    }
 
 
 }
