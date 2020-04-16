3279a7a4fc8cdab74961e419e10fd5716afd9a21
omar suliman abu sabha <ssomar2028@gmail.com>
2020-04-16 23:32:59 +0300

    15th day: solve checkValidString() problem by greedy approach

--- src/com/company/Contests/Week_3_leetCodeContest_30Days.java
+++ src/com/company/Contests/Week_3_leetCodeContest_30Days.java
@@ -1,15 +1,23 @@
 package com.company.Contests;
 
+import javafx.util.Pair;
+
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Week_3_leetCodeContest_30Days {
 
     public static void main(String[] args) {
 
-        int[] temp = productExceptSelf(new int[]{1,2,3,4});
+        //14th day
+      /*  int[] temp = productExceptSelf(new int[]{1,2,3,4});
         for (int i = 0; i < temp.length; i++) {
             System.out.print(temp[i]+ " ");
         }
+        */
+
+        //15th day
+        System.out.println(checkValidString("(*))"));;
 
 
     }
@@ -56,4 +64,53 @@ public class Week_3_leetCodeContest_30Days {
         return ans;
     }
 
+    /**
+     * 15th day :Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+     *
+     * Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+     * Any right parenthesis ')' must have a corresponding left parenthesis '('.
+     * Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+     * '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+     * An empty string is also valid.
+     **/
+
+
+    // not correct answer there some cases not accept it lol -_-
+  /*  public static boolean checkValidString(String s) {
+        Stack<Character> tracer = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            if ( s.charAt(i) == '(' ){
+                tracer.push(s.charAt(i));
+                System.out.printf("i = %d  tracer = %s \n",i,tracer.toString());
+            } else{
+                if(tracer.isEmpty()){
+                    System.out.println("hack");
+                    return false;
+                }
+                char lastValue = tracer.peek();
+                if( (s.charAt(i)== ')' && lastValue == '(') ||  (s.charAt(i)== '*' && lastValue == '(') || (s.charAt(i)== '*' && lastValue == ')')) {
+                    tracer.pop();
+                   System.out.printf("i = %d  tracer = %s \n",i,tracer.toString());
+                } else {
+                    break;
+                }
+            }
+        }
+        return tracer.size() == 0 ;
+
+    }*/
+
+
+  // == >   (*)) solve it by Greedy  approach
+
+    public static boolean checkValidString(String s) {
+        int leftPer = 0, rightPer = 0;
+        for (char ch: s.toCharArray()) {
+            leftPer += (ch == '(' ? 1 : -1);
+            rightPer += (ch != ')' ? 1 : -1);
+            if (rightPer < 0) break;
+            leftPer = Math.max(leftPer, 0);
+        }
+        return leftPer == 0;
+    }
 }
