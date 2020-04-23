2ccc2491190fa25ac2b8e080332b302e8af20b64
omar suliman abu sabha <ssomar2028@gmail.com>
2020-04-23 22:16:18 +0300

    22th day: solve rangeBitwiseAnd problem

--- src/com/company/Contests/LeetCode/Week_4_leetCodeContest_30Days.java
+++ src/com/company/Contests/LeetCode/Week_4_leetCodeContest_30Days.java
@@ -4,8 +4,10 @@ public class Week_4_leetCodeContest_30Days {
 
     public static void main(String[] args) {
         // 21th day:
-        System.out.println(subarraySum(new int[]{1,2,2},2));
+        // System.out.println(subarraySum(new int[]{1,2,2},2));
 
+        // 22th day:
+        System.out.println(rangeBitwiseAnd(5,7));
 
     }
 
@@ -32,4 +34,22 @@ public class Week_4_leetCodeContest_30Days {
         }
         return count;
     }
+
+    /** 22th day:
+     Given an array of integers and an integer k, you need to find the total number of
+     continuous subarrays whose sum equals to k.
+     */
+
+    public static int rangeBitwiseAnd(int m, int n) {
+        if(m == 0){
+            return 0;
+        }
+        while(n > m){
+            n = n & n-1;
+        //    System.out.println(n);
+        }
+        return  n;
+    }
+
+
 }
