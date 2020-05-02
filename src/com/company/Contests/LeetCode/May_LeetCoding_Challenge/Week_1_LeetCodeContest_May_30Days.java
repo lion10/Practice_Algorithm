10af9eab81b1e825f804896d22f0016df680325c
omar suliman abu sabha <ssomar2028@gmail.com>
2020-05-02 18:27:46 +0300

    2nd day : solve numJewelsInStones problem

--- src/com/company/Contests/LeetCode/May_LeetCoding_Challenge/Week_1_LeetCodeContest_May_30Days.java
+++ src/com/company/Contests/LeetCode/May_LeetCoding_Challenge/Week_1_LeetCodeContest_May_30Days.java
@@ -4,6 +4,8 @@ public class Week_1_LeetCodeContest_May_30Days {
 
     public static void main(String[] args) {
 
+        //day 2
+        System.out.println(numJewelsInStones("aA","aAAbbbb"));
 
 
     }
@@ -50,6 +52,30 @@ public class Week_1_LeetCodeContest_May_30Days {
 //    }
 
 
+
+
+
+    // Day2
+    /**
+     * You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character
+     * in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+     * The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive,
+     * so "a" is considered a different type of stone from "A".
+     * */
+
+    public static int numJewelsInStones(String J, String S) {
+
+        int count = 0;
+
+        for (int i = 0; i <J.length() ; i++) {
+            for (int j = 0; j < S.length() ; j++) {
+                if(J.charAt(i) == S.charAt(j))
+                    count++;
+            }
+        }
+
+        return count;
+    }
 
 
 
