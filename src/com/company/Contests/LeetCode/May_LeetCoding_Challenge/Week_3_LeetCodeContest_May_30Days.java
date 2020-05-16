a8593e7cd6442241673fe26e51feb1961313bb8a
omar suliman abu sabha <ssomar2028@gmail.com>
2020-05-16 16:49:25 +0300

    Day 16:solved oddEvenList() problem

--- src/com/company/Contests/LeetCode/May_LeetCoding_Challenge/Week_3_LeetCodeContest_May_30Days.java
+++ src/com/company/Contests/LeetCode/May_LeetCoding_Challenge/Week_3_LeetCodeContest_May_30Days.java
@@ -54,5 +54,40 @@ public class Week_3_LeetCodeContest_May_30Days {
         return maxSoFar;
     }
 
+    // Day 16
+    /**Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we
+     *are talking about the node number and not the value in the nodes.
+     You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.*/
 
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    class Solution {
+        public ListNode oddEvenList(ListNode head) {
+            if (head == null) {
+                return head;
+            }
+            ListNode node = head;
+            ListNode last = head;
+            int len = 1;
+            while(last.next!= null){
+                last = last.next;
+                len++;
+            }
+            for(int i = 0; i < len/2; i++){
+                last.next = node.next;
+                last = last.next;
+                node.next = node.next.next;
+                node = node.next;
+                last.next = null;
+            }
+            return head;
+
+        }
+    }
 }
