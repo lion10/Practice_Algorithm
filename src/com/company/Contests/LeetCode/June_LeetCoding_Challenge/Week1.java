5a88900474bfbbeb65942b4db0dc88875cce74de
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-02 06:02:10 +0300

    Start new Challenge-Day 1: invertTree() problem

---
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week1.java
@@ -0,0 +1,35 @@
+package com.company.Contests.LeetCode.June_LeetCoding_Challenge;
+
+public class Week1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Day 1
+    /** Invert a binary tree.*/
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null)
+            return null;
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+
+}
