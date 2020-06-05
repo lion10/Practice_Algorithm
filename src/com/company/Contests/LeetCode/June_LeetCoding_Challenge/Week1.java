b190dd0309776687e753dac940bbea44d75e896c
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-05 04:40:50 +0300

    Day 4: reverseString() problem

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week1.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week1.java
@@ -67,5 +67,24 @@ public class Week1 {
 
     }
 
+    // Day 4
+    /** Write a function that reverses a string. The input string is given as an array of characters char[].
+
+     Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+     You may assume all the characters consist of printable ascii characters.*/
+
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+        while (i < j)
+            swap (s, i++, j--);
+    }
+
+    public void swap(char[] s, int i, int j) {
+        char c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+
 
 }
