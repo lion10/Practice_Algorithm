89d2a8a8bc5d108adab0198fdfb999a9d781fdba
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-07 08:33:13 +0300

    Day 6: reconstructQueue()

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week1.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week1.java
@@ -1,6 +1,8 @@
 package com.company.Contests.LeetCode.June_LeetCoding_Challenge;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Week1 {
 
@@ -103,13 +105,40 @@ public class Week1 {
                 probabilities[i] = w[i]/sum;
             }
         }
-
         public int pickIndex() {
             return Math.abs(Arrays.binarySearch(this.probabilities, Math.random())) - 1;
         }
-
-
     }
 
+    // Day 6
+    /** Suppose you have a random list of people standing in a queue. Each person is described by a pair
+     *  of integers (h, k), where h is the height of the person and k is the number of people in front of
+     *  this person who have a height greater than or equal to h.
+     *  Write an algorithm to reconstruct the queue.*/
+
+    public int[][] reconstructQueue(int[][] people) {
+
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                if (a[0] != b[0])
+                    return b[0] - a[0];
+                else
+                    return a[1] - b[1];
+            }
+        });
+
+        ArrayList<int[]> tmp = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            tmp.add(people[i][1], people[i]);
+        }
+
+        int[][] res = new int[people.length][2];
+        for (int i = 0; i < tmp.size(); i++) {
+            res[i][0] = tmp.get(i)[0];
+            res[i][1] = tmp.get(i)[1];
+        }
+
+        return res;
+    }
 }
 
