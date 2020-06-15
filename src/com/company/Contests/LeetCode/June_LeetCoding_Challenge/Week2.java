ae3ea8c608c9d008ccfd305768554dd4d01dc801
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-15 09:06:25 +0300

    Day 14 - June: findCheapestPrice() problem

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week2.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week2.java
@@ -199,4 +199,33 @@ public class Week2 {
 
     }
 
+
+    // Day 14
+    /** There are n cities connected by m flights. Each flight starts from city u and arrives at v with a
+     *  price w.
+     Now given all the cities and flights, together with starting city src and the destination
+     dst, your task is to find the cheapest price from src to dst with up to k stops.
+     If there is no such route, output -1.*/
+
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        for (int i = 0; i <= K; i++) {
+            int[] tempDist = Arrays.copyOf(dist, dist.length);
+            for (int[] edge : flights) {
+                int u = edge[0];
+                if (dist[u] == Integer.MAX_VALUE)
+                    continue;
+                int v = edge[1];
+                int w = edge[2];
+                tempDist[v] = Math.min(tempDist[v], dist[u] + w);
+            }
+            dist = tempDist;
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+
+    }
 }
