cc754233bdbfcb592db72edc99b19137326a45a9
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-18 09:40:43 +0300

    Day 17: surroundedRegions problem

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week3.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week3.java
@@ -79,6 +79,71 @@ public class Week3 {
         return result;
     }
 
+    //Day 17
+
+    /** Given a 2D board containing 'X' and 'O' (the letter O), capture all regions surrounded by 'X'.
+
+     A region is captured by flipping all 'O's into 'X's in that surrounded region.*/
+
+    public void solve(char[][] board) {
+        if(board.length == 0) return;
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        // traverse edge to turn O on edge and its relatives to *
+        for(int i = 0; i < rows; i++){
+            if(board[i][0] == 'O'){
+                dfs(board, i, 0);
+            }
+
+            if(board[i][cols-1] == 'O'){
+                dfs(board, i, cols-1);
+            }
+        }
+
+        for(int i = 0; i < cols; i++){
+            if(board[0][i] == 'O'){
+                dfs(board, 0, i);
+            }
+
+            if(board[rows-1][i] == 'O'){
+                dfs(board, rows-1, i);
+            }
+        }
+
+        // flip O to X
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(board[i][j] == 'O'){
+                    board[i][j] = 'X';
+                }else if(board[i][j] == '*'){
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+
+    public void dfs(char[][] board, int row, int col){
+        if(row < 0 || row >= board.length || col < 0 || col >= board[0].length){
+            return;
+        }
+
+        if(board[row][col] == 'O')
+            board[row][col] = '*';
+        // top
+        if(row-1 >= 0 && board[row-1][col] == 'O')
+            dfs(board, row-1, col);
+        // bottom
+        if(row+1 < board.length && board[row+1][col] == 'O')
+            dfs(board, row+1, col);
+        // left
+        if(col-1 >= 0 && board[row][col-1] == 'O')
+            dfs(board, row, col-1);
+        // right
+        if(col+1 < board[0].length && board[row][col+1] == 'O')
+            dfs(board, row, col+1);
+    }
 
 
 
