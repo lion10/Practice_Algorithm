b092fbfc209abdc70e12ceedaf8d1084b71c4903
omar suliman abu sabha <ssomar2028@gmail.com>
2020-06-20 09:12:58 +0300

    Day 19- June: longestDubSubstring() problem

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week3.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week3.java
@@ -1,5 +1,8 @@
 package com.company.Contests.LeetCode.June_LeetCoding_Challenge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Week3 {
 
     public static void main(String[] args) {
@@ -167,5 +170,67 @@ public class Week3 {
         }
         return n - i - 1;
     }
+
+    // Day 19
+
+    /** Given a string S, consider all duplicated substrings: (contiguous) substrings of S that occur 2 or more times.
+     *  (The occurrences may overlap.)
+     Return any duplicated substring that has the longest possible length.  (If S does not have a duplicated substring,
+     the answer is "".)*/
+
+    public String longestDupSubstring(String S) {
+        int left = 0;
+        int right = S.length() - 1;
+
+        while (left < right) {
+            int mid = left + ((right - left + 1) >> 1); // that's mean divide on 2 when the loop are iterated
+
+            if (isDuplicatePresent(S, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return findDuplicate(S, left);
+    }
+
+    private boolean isDuplicatePresent(String S, int length) {
+        if (length == 0)
+            return true;
+
+        return findDuplicate(S, length) != null;
+    }
+
+    private String findDuplicate(String S, int length) {
+        long hash = 0;
+        long prime = 29;
+        long firstEntryPower = 1;
+        for (int i = 0; i < length; i++) {
+            firstEntryPower *= prime;
+            hash = hash * prime + (S.charAt(i) - 'a');
+        }
+
+        Map<Long, Integer> map = new HashMap<>();
+        map.put(hash, 0);
+
+        for (int i = length; i < S.length(); i++) {
+            hash = hash * prime + (S.charAt(i) - 'a');
+            hash -= firstEntryPower * (S.charAt(i - length) - 'a');
+
+            if (map.containsKey(hash)) {
+                int index = map.get(hash);
+                return S.substring(index, index + length);
+            }
+
+            map.put(hash, i - length + 1);
+        }
+
+        return null;
+    }
+
+
+
+
 
 }
