f55e758f9f6626230a07987b3b4722af73ff8c21
omar suliman abu sabha <ssomar2028@gmail.com>
2020-07-01 09:46:58 +0300

    Day 30 - June: word search problem

--- src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week5.java
+++ src/com/company/Contests/LeetCode/June_LeetCoding_Challenge/Week5.java
@@ -1,5 +1,10 @@
 package com.company.Contests.LeetCode.June_LeetCoding_Challenge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Week5 {
     public static void main(String[] args) {
 
@@ -22,6 +27,57 @@ public class Week5 {
             }
         }
         return dp[m-1][n-1];
+    }
+
+
+    // Day 30:  Word Search II
+    /**Given a 2D board and a list of words from the dictionary, find all words in the board.
+     Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or
+     vertically neighboring. The same letter cell may not be used more than once in a word.*/
+
+    public List<String> findWords(char[][] board, String[] words) {
+        Map<Character, List<int[]>> map = new HashMap<>();
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                List<int[]> nodes = map.getOrDefault(board[i][j], new ArrayList<>());
+                nodes.add(new int[]{i, j});
+                map.put(board[i][j], nodes);
+            }
+        }
+        List<String> ans = new ArrayList<>();
+        for(String word : words){
+            for(int[] pos : map.getOrDefault(word.charAt(0), new ArrayList<>())){
+                if(containsWord(word, 1, pos[0], pos[1], board)){
+                    ans.add(word);
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+    private int[][] dirs = new int[][]{{1,0},{-1,0},{0,1},{0,-1}};
+    private boolean containsWord(String w, int idx, int x, int y, char[][] board){
+        if(idx >= w.length()) return true;
+        if(x < 0||y<0||x>=board.length||y>=board[0].length){
+            return false;
+        }
+        char oldChar = board[x][y];
+        board[x][y] = '#';
+        boolean findMatch = false;
+        for(int[] dir : dirs){
+            int newX = x + dir[0];
+            int newY = y + dir[1];
+            if(newX<0||newY<0||newX>=board.length||newY>=board[0].length||board[newX][newY]!=w.charAt(idx)){
+                continue;
+            }
+
+            if(containsWord(w, idx+1, newX, newY, board)){
+                findMatch = true;
+                break;
+            }
+        }
+        board[x][y] = oldChar;
+        return findMatch;
     }
 }
 
