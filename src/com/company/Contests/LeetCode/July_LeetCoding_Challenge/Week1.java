349133ad46b506c4ad905ff50eb664163280f960
omar suliman abu sabha <ssomar2028@gmail.com>
2020-07-04 10:09:12 +0300

    Day 3 -June: prison cells after N days problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week1.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week1.java
@@ -40,6 +40,8 @@ public class Week1 {
             this.right = right;
         }
     }
+
+
     /** Given a binary tree, return the bottom-up level order traversal of its nodes' values.
      *  (ie, from left to right, level by level from leaf to root).
      For example:
@@ -72,4 +74,34 @@ public class Week1 {
         return list;
     }
 
+    // Day 3 Prison Cells After N Days
+
+    /** There are 8 prison cells in a row, and each cell is either occupied or vacant.
+
+     Each day, whether the cell is occupied or vacant changes according to the following rules:
+
+     If a cell has two adjacent neighbors that are both occupied or both vacant, then the cell becomes occupied.
+     Otherwise, it becomes vacant.
+     (Note that because the prison is a row, the first and the last cells in the row can't have two adjacent neighbors.)
+
+     We describe the current state of the prison in the following way: cells[i] == 1 if the i-th cell is occupied, else cells[i] == 0.
+
+     Given the initial state of the prison, return the state of the prison after N days (and N such changes described above.)*/
+
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        N = (N-1) % 14 +1;
+        for (int i = 0; i < N ; i++) {
+            cells = newDay(cells);
+        }
+        return cells;
+
+    }
+
+    private int[] newDay(int[] cells) {
+        int[] result = new int[cells.length];
+        for (int i = 1; i < cells.length-1; i++) {
+            result[i] = cells[i-1] == cells[i+1] ? 1 :0;
+        }
+        return result;
+    }
 }
