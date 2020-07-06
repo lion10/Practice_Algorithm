27fa4d0251bf8874594f270512352a8ec1f5cc31
Omar Abusabha <ssomar2028@gmail.com>
2020-07-06 09:53:00 +0300

    Day 5- July: Hamming Distance Problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week1.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week1.java
@@ -127,4 +127,12 @@ public class Week1 {
 
         return dp[n-1];
     }
+
+    // Day 5 Hamming Distance
+    /** The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+     Given two integers x and y, calculate the Hamming distance.*/
+
+    public int hammingDistance(int x, int y) {
+        return Integer.bitCount(x ^ y);
+    }
 }
