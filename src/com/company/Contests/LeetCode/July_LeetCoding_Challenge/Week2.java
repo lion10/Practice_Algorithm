0d6c8e775d2bbc480d5cd4cc77e92c55331833fa
Omar Abusabha <ssomar2028@gmail.com>
2020-07-14 09:04:25 +0300

    Day 13 - July: Same Tree Problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week2.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week2.java
@@ -180,4 +180,15 @@ public class Week2 {
     }
 
 
+    // Day 13 Same Tree
+
+    /** Given two binary trees, write a function to check if they are the same or not.
+     Two binary trees are considered the same if they are structurally identical and the nodes have the same value.*/
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (p == null || q == null)
+            return false;
+        return q.val == p.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    }
 }
