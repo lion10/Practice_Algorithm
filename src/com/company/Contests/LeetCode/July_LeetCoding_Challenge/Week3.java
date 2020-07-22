eb2d822f13ca933338cdf887dc3ad3a115ee32c6
Omar Abusabha <ssomar2028@gmail.com>
2020-07-22 08:22:16 +0300

    Day 21 - July: Word Search problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week3.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week3.java
@@ -166,4 +166,46 @@ public class Week3 {
         }
         return head;
     }
+
+    // Day 21 Word Search
+
+    /** Given a 2D board and a word, find if the word exists in the grid.
+     The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally
+     or vertically neighboring. The same letter cell may not be used more than once.*/
+
+    static boolean[][] visited;
+    public boolean exist(char[][] board, String word) {
+        visited = new boolean[board.length][board[0].length];
+
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[i].length; j++){
+                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean search(char[][]board, String word, int i, int j, int index){
+        if(index == word.length()){
+            return true;
+        }
+
+        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
+            return false;
+        }
+
+        visited[i][j] = true;
+        if(search(board, word, i-1, j, index+1) ||
+                search(board, word, i+1, j, index+1) ||
+                search(board, word, i, j-1, index+1) ||
+                search(board, word, i, j+1, index+1)){
+            return true;
+        }
+
+        visited[i][j] = false;
+        return false;
+    }
 }
