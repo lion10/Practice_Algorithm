25709ae570a743bd8c4b12c28225139fbdab27a2
Omar Abusabha <ssomar2028@gmail.com>
2020-07-29 09:46:52 +0300

    Day 28 - July: Task Scheduler Problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week4.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week4.java
@@ -202,6 +202,41 @@ public class Week4 {
     }
 
 
+    // Day 28: Task Scheduler
+
+    /** You are given a char array representing tasks CPU need to do. It contains capital letters A to Z where
+     *  each letter represents a different task. Tasks could be done without the original order of the array.
+     *  Each task is done in one unit of time. For each unit of time, the CPU could complete either one task or just be idle.
+     However, there is a non-negative integer n that represents the cooldown period between two same tasks
+     (the same letter in the array), that is that there must be at least n units of time between any two same tasks.
+     You need to return the least number of units of times that the CPU will take to finish all the given tasks.*/
+
+    public int leastInterval(char[] tasks, int n) {
+        if (n == 0) return tasks.length;
+
+        Map<Character, Integer> taskToCount = new HashMap<>();
+        for (char c : tasks) {
+            taskToCount.put(c, taskToCount.getOrDefault(c, 0) + 1);
+        }
+
+        Queue<Integer> queue = new PriorityQueue<>((i1, i2) -> i2 - i1);
+        for (char c : taskToCount.keySet()) queue.offer(taskToCount.get(c));
+
+        Map<Integer, Integer> coolDown = new HashMap<>();
+        int currTime = 0;
+        while (!queue.isEmpty() || !coolDown.isEmpty()) {
+            if (coolDown.containsKey(currTime - n - 1)) {
+                queue.offer(coolDown.remove(currTime - n - 1));
+            }
+            if (!queue.isEmpty()) {
+                int left = queue.poll() - 1;
+                if (left != 0) coolDown.put(currTime, left);
+            }
+            currTime++;
+        }
+
+        return currTime;
+    }
 
 
 }
