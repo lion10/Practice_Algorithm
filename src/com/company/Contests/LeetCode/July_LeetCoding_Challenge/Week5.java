3237d9a6d92429813b60695a5bd50f4a9c98868d
Omar Abusabha <ssomar2028@gmail.com>
2020-08-01 09:43:49 +0300

    Day 31 - July: Climbing Stairs problem

--- src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week5.java
+++ src/com/company/Contests/LeetCode/July_LeetCoding_Challenge/Week5.java
@@ -80,4 +80,24 @@ public class Week5 {
         return ans;
     }
 
+
+    // Day 31: Climbing Stairs
+    /**
+     * You are climbing a stair case. It takes n steps to reach to the top.
+     * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     **/
+    public int climbStairs(int n) {
+        if(n == 1)
+            return 1;
+        int first = 1;
+        int secound = 2;
+        for(int i = 3 ; i <= n; i++){
+            int third = first+ secound;
+            first = secound;
+            secound = third;
+        }
+
+        return secound;
+    }
+
 }
