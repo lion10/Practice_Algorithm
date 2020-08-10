16f96174e2a88b46a9e293cb67b3723eb5407992
Omar Abusabha <ssomar2028@gmail.com>
2020-08-10 10:06:55 +0300

    Day - 9 - Auguest: Rotting Oranges

--- src/com/company/Contests/LeetCode/Augest_LeetCoding_Challenge/Week2.java
+++ src/com/company/Contests/LeetCode/Augest_LeetCoding_Challenge/Week2.java
@@ -1,5 +1,8 @@
 package com.company.Contests.LeetCode.Augest_LeetCoding_Challenge;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Week2 {
 
 
@@ -35,5 +38,69 @@ public class Week2 {
         int cur = 0;
         if(root.val == remain) cur = 1;
         return cur + process(root.left,remain - root.val) + process(root.right,remain -root.val);
+    }
+
+
+
+
+    //Day 9 : Rotting Oranges
+
+    /** In a given grid, each cell can have one of three values:
+     the value 0 representing an empty cell;
+     the value 1 representing a fresh orange;
+     the value 2 representing a rotten orange.
+     Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+     Return the minimum number of minutes that must elapse until no cell has a fresh orange.
+     If this is impossible, return -1 instead.
+     */
+    public int orangesRotting(int[][] grid) {
+        Queue<int[]> queue = new LinkedList<int[]>();
+        //add all the rotten orange coordinate to queue
+        //also, set rotten orange coordinate to 0 so we won't visit it again
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                    grid[i][j] = 0;
+                }
+            }
+        }
+        int stepCount = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                int[] currCoord = queue.remove();
+                int currRow = currCoord[0];
+                int currCol = currCoord[1];
+                if (currRow+1 < grid.length && grid[currRow+1][currCol] == 1) {
+                    grid[currRow+1][currCol] = 0;  //set to 0 so we won't visit it again
+                    queue.add(new int[]{currRow+1, currCol});
+                }
+                if (currRow-1 >= 0 && grid[currRow-1][currCol] == 1) {
+                    grid[currRow-1][currCol] = 0;
+                    queue.add(new int[]{currRow-1, currCol});
+                }
+                if (currCol+1 < grid[currRow].length && grid[currRow][currCol+1] == 1) {
+                    grid[currRow][currCol+1] = 0;
+                    queue.add(new int[]{currRow, currCol+1});
+                }
+                if (currCol-1 >= 0 && grid[currRow][currCol-1] == 1) {
+                    grid[currRow][currCol-1] = 0;
+                    queue.add(new int[]{currRow, currCol-1});
+                }
+                size--;
+            }
+            stepCount++;
+        }
+        //do we still have any fresh oranges in the grid
+        for (int m = 0; m < grid.length; m++) {
+            for (int n = 0; n < grid[m].length; n++) {
+                if (grid[m][n] == 1) {
+                    return -1;
+                }
+            }
+        }
+        //-1 because first iteration is all rotten oranges from the initial state
+        return Math.max(0, stepCount-1);
     }
 }
