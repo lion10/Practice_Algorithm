8769bb253cfcc49a1fe34e9a0989f934349ed1f4
Omar Abusabha <ssomar2028@gmail.com>
2020-08-31 20:04:20 +0300

    Day 30 - Aug: Largest Component Size by Common Factor

--- src/com/company/Contests/LeetCode/Augest_LeetCoding_Challenge/Week5.java
+++ src/com/company/Contests/LeetCode/Augest_LeetCoding_Challenge/Week5.java
@@ -1,6 +1,8 @@
 package com.company.Contests.LeetCode.Augest_LeetCoding_Challenge;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Week5 {
@@ -46,5 +48,58 @@ public class Week5 {
             rev(a, 0, n-1);
         }
         return result;
+    }
+
+
+
+
+    // Day 30: Largest Component Size by Common Factor
+    /** Given a non-empty array of unique positive integers A, consider the following graph:
+     There are A.length nodes, labelled A[0] to A[A.length - 1];
+     There is an edge between A[i] and A[j] if and only if A[i] and
+     A[j] share a common factor greater than 1.
+     Return the size of the largest connected component in the graph.
+     */
+
+    public int find(int x,int[] parent){
+        if(parent[x]==-1)
+            return x;
+        else
+            parent[x]=find(parent[x],parent);
+        return parent[x];
+    }
+    public void union(int x,int y,int[] parent){
+        int xp=find(x,parent);
+        int yp=find(y,parent);
+        if(xp!=yp)
+        {
+            parent[yp]=xp;
+        }
+        return;
+    }
+    public int largestComponentSize(int[] nums) {
+        int[] parent=new int[100001];
+        Arrays.fill(parent,-1);
+        int i;
+        for(i=0;i<nums.length;i++)
+        {
+            for(int k=2;k<=Math.sqrt(nums[i]);k++)
+            {
+                if(nums[i]%k==0)
+                {
+                    union(nums[i],k,parent);
+                    union(nums[i],nums[i]/k,parent);
+                }
+            }
+        }
+        int count=0;
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(i=0;i<nums.length;i++)
+        {
+            int p=find(nums[i],parent);
+            count=Math.max(count,map.getOrDefault(p,0)+1);
+            map.put(p,map.getOrDefault(p,0)+1);
+        }
+        return count;
     }
 }
